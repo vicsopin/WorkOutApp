55fceceaf80ac4a72c3a9f987d2b7a49836ed5d7
vicsopin <victor.sopin@student.lut.fi>
2023-06-04 08:43:32 +0300

    Initial Commit

---
+++ Work-Out/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Work-Out
+//
+//  Created by Victor Sopin on 04/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Work-Out/Work_OutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Work_OutApp.swift
+//  Work-Out
+//
+//  Created by Victor Sopin on 04/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Work_OutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
